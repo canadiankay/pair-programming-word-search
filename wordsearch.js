a20d56a1d698ad0a09827a6ccca31e28c5647639
canadiankay <kasimnadia@gmail.com>
2021-12-02 22:38:12 +0000

    minor edits

--- wordsearch.js
+++ wordsearch.js
@@ -1,6 +1,6 @@
 /*
 //This function fixes test 1 and 2 - if a word is horizontal + present or not present
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
@@ -10,43 +10,41 @@ const wordSearch = (letters, word) => {
 
 */
 
-const transpose = function(array){
-    let results = [];
-    for (let i = 0; i < array[0].length; i++) {
-        let row = [];
-        for (let j = 0; j < array.length; j++) {
-            row.push(array[j][i]);
-        }
-        results.push(row);
+const transpose = function(array) {
+  let results = [];
+  for (let i = 0; i < array[0].length; i++) {
+    let row = [];
+    for (let j = 0; j < array.length; j++) {
+      row.push(array[j][i]);
     }
-    return results
+    results.push(row);
+  }
+  return results;
 };
 
 
 //This function passes test 3 if the word is present and vertical- we need to use transpose (pasted right above this)
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+  if (letters.length < 1 || word.length < 1) return false; //takes care of whether a word is not present
     
-    if (letters.length < 1 || word.length < 1) return false; //takes care of whether a word is not present
-    
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    
-    for (l of horizontalJoin) {
-        if (l.includes(word)) {
-            return true
-        }
-    };
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
 
-    const vertical = transpose(letters)
-        const verticalJoin = vertical.map(ls => ls.join(''))
-            for (l of verticalJoin) {
-                if (l.includes(word)) {
-                    return true;
-                }
-            }
-        return false;
+  const vertical = transpose(letters);
+  const verticalJoin = vertical.map(ls => ls.join(''));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
 };
 
-module.exports = wordSearch
+module.exports = wordSearch;
 
 
 
