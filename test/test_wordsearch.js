f0541c07b70dbbe591199984673c650e43233a23
canadiankay <kasimnadia@gmail.com>
2021-12-02 15:16:16 +0000

    solution to word search pair programming activity

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -18,7 +18,7 @@ describe("#wordSearch()", function() {
     ], 'FRANK')
 
     assert.isFalse(result);
-  });
+  }); //this is an easy fix: make the original function return false
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
@@ -35,4 +35,21 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  //We also need to do a test if the world is vertical
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['U', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['M', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYLUM')
+
+    assert.isTrue(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,55 @@
+/*
+//This function fixes test 1 and 2 - if a word is horizontal + present or not present
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    return false; //if the word is not present
+};
+
+*/
+
+const transpose = function(array){
+    let results = [];
+    for (let i = 0; i < array[0].length; i++) {
+        let row = [];
+        for (let j = 0; j < array.length; j++) {
+            row.push(array[j][i]);
+        }
+        results.push(row);
+    }
+    return results
+};
+
+
+//This function passes test 3 if the word is present and vertical- we need to use transpose (pasted right above this)
+const wordSearch = (letters, word) => { 
+    
+    if (letters.length < 1 || word.length < 1) return false; //takes care of whether a word is not present
+    
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true
+        }
+    };
+
+    const vertical = transpose(letters)
+        const verticalJoin = vertical.map(ls => ls.join(''))
+            for (l of verticalJoin) {
+                if (l.includes(word)) {
+                    return true;
+                }
+            }
+        return false;
+};
 
 module.exports = wordSearch
+
+
+
+
+
+
